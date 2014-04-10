964e66e01ea4e6bcf245989bc4ae40edb9e4bf86
Gokul Evuri <gokul@evuri.com>
2014-04-10 23:32:42 +0530

    testing poll id

--- app/server/modules/betteroo_api.js
+++ app/server/modules/betteroo_api.js
@@ -62,7 +62,7 @@ function create_poll(question,option1,option2,dynamodb){
 	var poll_var =  {
             "TableName":"polls",
               "Item":{
-              "poll_id":{"N": poll_id_gen+''},
+              "poll_id":{"N": '1'},
               "total_votes":{"N":'0'},
               "total_views":{"N":'0'},
               "question":{"S":question},
