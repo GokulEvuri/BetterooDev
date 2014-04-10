e808f96fc892634e74d58db360335f2f6372847e
Gokul Evuri <gokul@evuri.com>
2014-04-11 01:52:38 +0530

    testig create post

--- app/server/modules/betteroo_api.js
+++ app/server/modules/betteroo_api.js
@@ -181,7 +181,7 @@ function cpdsa(question,option1,option2,dynamodb){
   var poll_var =  {
             "TableName":"polls",
               "Item":{
-              "poll_id":{"N": poll_id_gen+''},
+              "poll_id":{"N": poll_id_gen+1+''},
               "total_votes":{"N":'0'},
               "total_views":{"N":'0'},
               "question":{"S":question},
