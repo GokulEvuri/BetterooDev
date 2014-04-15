4c6eacd78ac3ca2db2c5f48998023e403e7c6082
Gokul Evuri <gokul@evuri.com>
2014-04-16 03:11:40 +0530

    syntax correction

--- app/server/modules/betteroo_api.js
+++ app/server/modules/betteroo_api.js
@@ -315,5 +315,5 @@ function cpdsa(question,option1,option2,dynamodb){
 //exports.is_uname_unieque = is_uname_unieque; //public function
 //exports.login = login;
 exports.create_poll = create_poll;
-exports.get_stats = get_stats;
+//exports.get_stats = get_stats;
 exports.vote = vote;
